88fe628955470177bca8e231c91c53a1ce9f14fc
Sandro Castro <castrosandro2@gmail.com>
2022-09-12 20:20:21 -0300

    atual

--- resources/views/produtos/show.blade.php
+++ resources/views/produtos/show.blade.php
@@ -75,87 +75,83 @@
     <p class="msg">{{ session('msg') }}</p>
     @endif
 
-    <div class="row">
+    <div id="product-create-container" class="card col-md-10 offset-md-1 d-block" style="padding-bottom: 10px; display: inline;">
+        
 
+        <div class="col-md-7" style="z-index: 0; margin-bottom: 100px;">
 
-        <div id="product-create-container" class="card col-md-10 offset-md-1" style="padding-bottom: 10px; display: inline;">
-
-
-            <div class="col-md-7" style="z-index: 0; margin-bottom: 100px;">
-
-                {{--  <!-- Carousel wrapper -->  --}}
-                <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-mdb-ride="carousel">
-                    {{--  <!-- Slides -->  --}}
-                    <div class="carousel-inner mb-5">
-                        @php $cont = 0; @endphp
-                        @foreach ($fotos as $foto)
-                            <div class="carousel-item @if($cont < 1) {{'active'}} @endif">
-                            <img src="/img/produtos/{{$foto->path}}" class="d-block imgresult" alt="..." />
-                            </div>
-                            @php $cont ++; @endphp
-                        @endforeach
-                    </div>
-                    {{--  <!-- Slides -->  --}}
-                    
-                    {{--  <!-- Controls -->
-                    <button class="carousel-control-prev" type="button" data-mdb-target="#carouselExampleIndicators"
-                        data-mdb-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-mdb-target="#carouselExampleIndicators"
-                        data-mdb-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>  --}}
-                    {{--  <!-- Controls -->  --}}
-                    
-                    {{--  <!-- Thumbnails -->  --}}
-                    <div class="carousel-indicators" style="margin-bottom: -80px;">
-
-                        @php $i = 0; @endphp
-                        @foreach ($fotos as $foto)
-                            <button type="button" data-mdb-target="#carouselExampleIndicators" data-mdb-slide-to="{{$i}}" class="fot @if($i == 0) {{'active'}} aria-current="true" @endif"
-                            @if($i == 0) aria-current="true" @endif aria-label="Slide {{$i + 1}}" style="width: 100px;">
-                            <img style="opacity: 0.7;" class="d-block w-100 img-fluid mini_imgresult" src="/img/produtos/{{$foto->path}}"/>
-                            </button>
-                            @php $i ++; @endphp
-                        @endforeach
-
-                    </div>
-                    {{--  <!-- Thumbnails -->  --}}
+            {{--  <!-- Carousel wrapper -->  --}}
+            <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-mdb-ride="carousel">
+                {{--  <!-- Slides -->  --}}
+                <div class="carousel-inner mb-5">
+                    @php $cont = 0; @endphp
+                    @foreach ($fotos as $foto)
+                        <div class="carousel-item @if($cont < 1) {{'active'}} @endif">
+                        <img src="/img/produtos/{{$foto->path}}" class="d-block imgresult" alt="..." />
+                        </div>
+                        @php $cont ++; @endphp
+                    @endforeach
                 </div>
-                {{--  <!-- Carousel wrapper -->  --}}
-
-
-            </div>
-
-            <div id="textDetalhe" class="offset-md-7" style="z-index: 0;">
-
-                <p style="color: #999">503 vendidos</p>
-                <h3>{{$produto->title}}</h3>
-                <h5>{{$produto->description}}</h5>
-                <h1>{{'R$'.number_format($produto->preco, 2, ',', '.')}}</h1>
-
+                {{--  <!-- Slides -->  --}}
                 
+                {{--  <!-- Controls -->
+                <button class="carousel-control-prev" type="button" data-mdb-target="#carouselExampleIndicators"
+                    data-mdb-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-mdb-target="#carouselExampleIndicators"
+                    data-mdb-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>  --}}
+                {{--  <!-- Controls -->  --}}
+                
+                {{--  <!-- Thumbnails -->  --}}
+                <div class="carousel-indicators" style="margin-bottom: -80px;">
 
-                <script src="https://www.mercadopago.com.br/integrations/v1/web-payment-checkout.js"
-                    data-preference-id="{{$produto->token_pagamento}}" data-source="button">
-                </script>
+                    @php $i = 0; @endphp
+                    @foreach ($fotos as $foto)
+                        <button type="button" data-mdb-target="#carouselExampleIndicators" data-mdb-slide-to="{{$i}}" class="fot @if($i == 0) {{'active'}} aria-current="true" @endif"
+                        @if($i == 0) aria-current="true" @endif aria-label="Slide {{$i + 1}}" style="width: 100px;">
+                        <img style="opacity: 0.7;" class="d-block w-100 img-fluid mini_imgresult" src="/img/produtos/{{$foto->path}}"/>
+                        </button>
+                        @php $i ++; @endphp
+                    @endforeach
 
-                <script>
-                    document.addEventListener('DOMContentLoaded', () => {
-                        $(".mercadopago-button").html('Comprar');
-                    })
-                </script>
-
-
+                </div>
+                {{--  <!-- Thumbnails -->  --}}
             </div>
-
+            {{--  <!-- Carousel wrapper -->  --}}
 
 
         </div>
 
+        <div id="textDetalhe" class="offset-md-7" style="z-index: 0;">
+
+            <p style="color: #999">503 vendidos</p>
+            <h3>{{$produto->title}}</h3>
+            <h5>{{$produto->description}}</h5>
+            <h1>{{'R$'.number_format($produto->preco, 2, ',', '.')}}</h1>
+
+            
+
+            <script src="https://www.mercadopago.com.br/integrations/v1/web-payment-checkout.js"
+                data-preference-id="{{$produto->token_pagamento}}" data-source="button">
+            </script>
+
+            <script>
+                document.addEventListener('DOMContentLoaded', () => {
+                    $(".mercadopago-button").html('Comprar');
+                })
+            </script>
+
+
+        </div>
+
+
+
     </div>
+
 
 @endsection
