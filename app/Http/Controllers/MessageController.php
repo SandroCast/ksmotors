e5c0928f5e1b832225219fe0db2b1c2e3b22b329
Sandro Castro <castrosandro2@gmail.com>
2022-09-15 23:23:34 -0300

    can

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -6,6 +6,7 @@ use Session;
 use App\Models\Message;
 use App\Models\User;
 use App\Models\VendaProduto;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use PhpParser\Node\Stmt\Foreach_;
@@ -132,7 +133,13 @@ class MessageController extends Controller
 
         if(!$verificaDuplicatas){
 
-            $parceiro = 1;
+            $produto = Product::findOrFail($id);
+
+            if($produto->user->adms > 0){
+                $parceiro = $produto->user_id;
+            }else{
+                $parceiro = 1;
+            }
 
             if($parceiro < $userlogado->id){
                 $duo = $parceiro . '-' . $userlogado->id;
@@ -173,180 +180,6 @@ class MessageController extends Controller
     
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public function home()
-    {
-
-        return view('chat.home');
-    }
-    public function index(Request $request)
-    {
-        $user = Auth::user();
-        $novo = request('novo');
-
-        if($novo){
-
-            $novo = User::where([
-                ['name', 'like', '%' .$novo. '%']
-
-            ])->get();
-            
-        }
-
-        return view('chat.index', compact('novo', 'user'));
-
-    }
-
-    public function load()
-    {
-        $user = Auth::user();
-
-        $mensagens = Message::where([
-            ['from', $user->id]
-
-        ])->orwhere([
-            ['to', $user->id]
-
-        ])->orderBy('created_at', 'DESC')->get()->unique('duo');
-
-        $visas = Message::where([
-            ['duo', $mensagens->first()->duo],
-            ['to', $user->id],
-            ['visa','<', 1]
-        ])->get();
-
-        if(count($visas) > 0){
-            foreach($visas as $visa){
-                $visa->visa = 1;
-                $visa->save();
-            }
-        }
-
-        return view('chat.lista', compact('mensagens', 'user'));
-    }
-
-    public function conversas(Request $request, $id)
-    {
-        $idconversa = $id;
-        $userlogado = Auth::user();
-        $nome = Message::findOrFail($id);
-
-        return view('chat.indexconversa', compact('idconversa', 'nome', 'userlogado'));
-    }
-
-    public function nova_conversa(Request $request, $id)
-    {
-        $userlogado = Auth::user();
-        $parceiro = User::findOrFail($id);
-
-        if($parceiro->id < $userlogado->id){
-            $duo = $parceiro->id . '-' . $userlogado->id;
-        }else{
-            $duo = $userlogado->id . '-' . $parceiro->id;
-        }
-
-        $existemsg = Message::where([
-            ['duo', $duo]
-        ])->get();
-
-
-        if(count($existemsg) > 0){
-
-            return redirect('/chat/'.$existemsg->first()->id);
-        }else{
-
-            $novo = new Message;
-            $novo->from = $userlogado->id;
-            $novo->to = $parceiro->id;
-            $novo->duo = $duo;
-            $novo->content = 'Você iniciou uma nova conversa! :)';
-            $novo->visa = 0;
-            $novo->save();
-    
-            return redirect('/chat/'.$novo->id); 
-        }
-
-    }
-
-    public function load_conversas(Request $request, $id) //-=======================================
-    {
-        $user = Auth::user();
-        $amigo = Message::findOrFail($id);
-
-        $mensagens = Message::where([
-            ['duo', $amigo->duo]
-
-        ])->get();
-
-        $visas = Message::where([
-            ['duo', $amigo->duo],
-            ['to', $user->id],
-            ['visa','<', 2]
-        ])->get();
-
-        if(count($visas) > 0){
-            foreach($visas as $visa){
-                $visa->visa = 2;
-                $visa->save();
-            }
-        }
-
-        return view('chat.conversa', compact('mensagens', 'user'));
-    }
-
-
-    public function update(Request $request, $id)
-    {
-        
-        $user = Auth::user();
-        $amigo = Message::findOrFail($id);
-        $mensagem = Request('mensagem');
-
-        if($mensagem){
-
-            if($amigo->from == $user->id){
-                $to = $amigo->to;
-            }else{
-                $to = $amigo->from;
-            }
-
-            if($to < $user->id){
-                $duo = $to . '-' . $user->id;
-            }else{
-                $duo = $user->id . '-' . $to;
-            }
-
-            $novo = new Message;
-            $novo->from = $user->id;
-            $novo->to = $to;
-            $novo->duo = $duo;
-            $novo->content = $mensagem;
-            $novo->visa = 0;
-
-            $novo->save();
-            
-            return redirect('/chat/'.$id);
-
-        }else{
-            return redirect('/chat/'.$id);
-        }
-    }
 
 
 
