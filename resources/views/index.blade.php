cb0871cc30133d01511acb0b9f6a4dec9751cefc
SandroCast <castrosandro2@gmail.com>
2023-12-28 21:36:17 -0300

    sal

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,6 +23,7 @@
             border-radius: 8px; /* Borda arredondada */
             overflow: hidden; /* Para esconder qualquer conteúdo que ultrapasse o card */
             transition: transform 0.3s ease; /* Adiciona uma transição suave de transformação */
+            height: 380px;
         }
 
         .card:hover {
@@ -32,21 +33,33 @@
         /* Estilização da barra de rolagem */
         .card-container::-webkit-scrollbar {
             width: 12px;
+            height: 5px;
         }
 
         .card-container::-webkit-scrollbar-thumb {
-            background-color: #888;
+            background-color: #ECF0F5;
             border-radius: 6px;
             transition: background-color 0.3s ease; /* Adiciona uma transição suave para a cor de fundo da barra de rolagem */
         }
 
         .card-container:hover::-webkit-scrollbar-thumb {
-            background-color: #ddd; /* Altera a cor de fundo da barra de rolagem ao passar o mouse sobre o contêiner */
+            background-color: #ECF0F5; /* Altera a cor de fundo da barra de rolagem ao passar o mouse sobre o contêiner */
         }
 
         h1{
             color:#444;
             margin-left: 20px;
+        }
+
+        .card-img-top{
+            width: 286px;
+            height: 286px;
+        }
+
+        .card-img-top {
+            width: 286px;
+            height: 286px;
+            object-fit: cover;
         }
 
     </style>
@@ -58,66 +71,144 @@
         <br>
         <h1>Baterias</h1>
     
-        <div class="card-container" id="cardContainer">
+        <div class="card-container cardContainer">
             <div class="card" style="width: 18rem;">
                 <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQnhpBqiKzRi_yASoZ_Jmouqq2KcBxLWopJbBqmngFa0JA_DCchOGjhMUSnnXqKLuI54QiWerAbOO4KPMxorQKm1J8ASFd8J0I8E9CSm_WWJv1twkurvE21&usqp=CAE" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title">Card title</h5>
+                    <span style=" font-size: 20px; font-weight: 600">R$ 500,00</span>
+                    <span style="float:right">5 dispovível</span>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="https://a-static.mlcdn.com.br/450x450/bateria-6v-12ah-para-moto-eletrica-infantil-brinquedos-moto-eletrica-carrinho-eletrico-bandeirante-sec-power/manialaser/58310bae771b11ec86974201ac18503a/428948c79b723bfecc10327ed1fbcc41.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <span style=" font-size: 20px; font-weight: 600">R$ 500,00</span>
+                    <span style="float:right">5 dispovível</span>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="https://a-static.mlcdn.com.br/450x450/bateria-unipower-6v-45ah-up645seg-moto-eletrica-carrinho-infantil-brinquedos/armazemsilvestre/957/b80665793674a5ae66966d8d8fc00b14.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <span style=" font-size: 20px; font-weight: 600">R$ 500,00</span>
+                    <span style="float:right">5 dispovível</span>
                 </div>
             </div>
             <div class="card" style="width: 18rem;">
                 <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQnhpBqiKzRi_yASoZ_Jmouqq2KcBxLWopJbBqmngFa0JA_DCchOGjhMUSnnXqKLuI54QiWerAbOO4KPMxorQKm1J8ASFd8J0I8E9CSm_WWJv1twkurvE21&usqp=CAE" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title">Card title</h5>
+                    <span style=" font-size: 20px; font-weight: 600">R$ 500,00</span>
+                    <span style="float:right">5 dispovível</span>
                 </div>
             </div>
             <div class="card" style="width: 18rem;">
                 <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQnhpBqiKzRi_yASoZ_Jmouqq2KcBxLWopJbBqmngFa0JA_DCchOGjhMUSnnXqKLuI54QiWerAbOO4KPMxorQKm1J8ASFd8J0I8E9CSm_WWJv1twkurvE21&usqp=CAE" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title">Card title</h5>
+                    <span style=" font-size: 20px; font-weight: 600">R$ 500,00</span>
+                    <span style="float:right">5 dispovível</span>
                 </div>
             </div>
             <div class="card" style="width: 18rem;">
                 <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQnhpBqiKzRi_yASoZ_Jmouqq2KcBxLWopJbBqmngFa0JA_DCchOGjhMUSnnXqKLuI54QiWerAbOO4KPMxorQKm1J8ASFd8J0I8E9CSm_WWJv1twkurvE21&usqp=CAE" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title">Card title</h5>
+                    <span style=" font-size: 20px; font-weight: 600">R$ 500,00</span>
+                    <span style="float:right">5 dispovível</span>
                 </div>
             </div>
             <div class="card" style="width: 18rem;">
                 <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQnhpBqiKzRi_yASoZ_Jmouqq2KcBxLWopJbBqmngFa0JA_DCchOGjhMUSnnXqKLuI54QiWerAbOO4KPMxorQKm1J8ASFd8J0I8E9CSm_WWJv1twkurvE21&usqp=CAE" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title">Card title</h5>
+                    <span style=" font-size: 20px; font-weight: 600">R$ 500,00</span>
+                    <span style="float:right">5 dispovível</span>
                 </div>
             </div>
             <div class="card" style="width: 18rem;">
                 <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQnhpBqiKzRi_yASoZ_Jmouqq2KcBxLWopJbBqmngFa0JA_DCchOGjhMUSnnXqKLuI54QiWerAbOO4KPMxorQKm1J8ASFd8J0I8E9CSm_WWJv1twkurvE21&usqp=CAE" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title">Card title</h5>
+                    <span style=" font-size: 20px; font-weight: 600">R$ 500,00</span>
+                    <span style="float:right">5 dispovível</span>
+                </div>
+            </div>
+
+
+
+        </div>
+
+        <h1>Lanternas</h1>
+    
+        <div class="card-container cardContainer">
+            <div class="card" style="width: 18rem;">
+                <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQnhpBqiKzRi_yASoZ_Jmouqq2KcBxLWopJbBqmngFa0JA_DCchOGjhMUSnnXqKLuI54QiWerAbOO4KPMxorQKm1J8ASFd8J0I8E9CSm_WWJv1twkurvE21&usqp=CAE" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <span style=" font-size: 20px; font-weight: 600">R$ 500,00</span>
+                    <span style="float:right">5 dispovível</span>
                 </div>
             </div>
             <div class="card" style="width: 18rem;">
                 <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQnhpBqiKzRi_yASoZ_Jmouqq2KcBxLWopJbBqmngFa0JA_DCchOGjhMUSnnXqKLuI54QiWerAbOO4KPMxorQKm1J8ASFd8J0I8E9CSm_WWJv1twkurvE21&usqp=CAE" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title">Card title</h5>
+                    <span style=" font-size: 20px; font-weight: 600">R$ 500,00</span>
+                    <span style="float:right">5 dispovível</span>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQnhpBqiKzRi_yASoZ_Jmouqq2KcBxLWopJbBqmngFa0JA_DCchOGjhMUSnnXqKLuI54QiWerAbOO4KPMxorQKm1J8ASFd8J0I8E9CSm_WWJv1twkurvE21&usqp=CAE" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <span style=" font-size: 20px; font-weight: 600">R$ 500,00</span>
+                    <span style="float:right">5 dispovível</span>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQnhpBqiKzRi_yASoZ_Jmouqq2KcBxLWopJbBqmngFa0JA_DCchOGjhMUSnnXqKLuI54QiWerAbOO4KPMxorQKm1J8ASFd8J0I8E9CSm_WWJv1twkurvE21&usqp=CAE" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <span style=" font-size: 20px; font-weight: 600">R$ 500,00</span>
+                    <span style="float:right">5 dispovível</span>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQnhpBqiKzRi_yASoZ_Jmouqq2KcBxLWopJbBqmngFa0JA_DCchOGjhMUSnnXqKLuI54QiWerAbOO4KPMxorQKm1J8ASFd8J0I8E9CSm_WWJv1twkurvE21&usqp=CAE" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <span style=" font-size: 20px; font-weight: 600">R$ 500,00</span>
+                    <span style="float:right">5 dispovível</span>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQnhpBqiKzRi_yASoZ_Jmouqq2KcBxLWopJbBqmngFa0JA_DCchOGjhMUSnnXqKLuI54QiWerAbOO4KPMxorQKm1J8ASFd8J0I8E9CSm_WWJv1twkurvE21&usqp=CAE" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <span style=" font-size: 20px; font-weight: 600">R$ 500,00</span>
+                    <span style="float:right">5 dispovível</span>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQnhpBqiKzRi_yASoZ_Jmouqq2KcBxLWopJbBqmngFa0JA_DCchOGjhMUSnnXqKLuI54QiWerAbOO4KPMxorQKm1J8ASFd8J0I8E9CSm_WWJv1twkurvE21&usqp=CAE" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <span style=" font-size: 20px; font-weight: 600">R$ 500,00</span>
+                    <span style="float:right">5 dispovível</span>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQnhpBqiKzRi_yASoZ_Jmouqq2KcBxLWopJbBqmngFa0JA_DCchOGjhMUSnnXqKLuI54QiWerAbOO4KPMxorQKm1J8ASFd8J0I8E9CSm_WWJv1twkurvE21&usqp=CAE" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <span style=" font-size: 20px; font-weight: 600">R$ 500,00</span>
+                    <span style="float:right">5 dispovível</span>
                 </div>
             </div>
         </div>
-
-
         
 
 
@@ -128,33 +219,38 @@
 
     <script>
 
-        let isMouseDown = false;
-        let startX;
-        let scrollLeft;
+        document.addEventListener('DOMContentLoaded', function() {
+            const cardContainers = document.querySelectorAll('.card-container');
 
-        const cardContainer = document.getElementById('cardContainer');
+            cardContainers.forEach(function(cardContainer) {
+                let isMouseDown = false;
+                let startX;
+                let scrollLeft;
 
-        cardContainer.addEventListener('mousedown', (e) => {
-            isMouseDown = true;
-            startX = e.pageX - cardContainer.offsetLeft;
-            scrollLeft = cardContainer.scrollLeft;
+                cardContainer.addEventListener('mousedown', function(e) {
+                    isMouseDown = true;
+                    startX = e.pageX - cardContainer.offsetLeft;
+                    scrollLeft = cardContainer.scrollLeft;
+                });
+
+                cardContainer.addEventListener('mouseleave', function() {
+                    isMouseDown = false;
+                });
+
+                cardContainer.addEventListener('mouseup', function() {
+                    isMouseDown = false;
+                });
+
+                cardContainer.addEventListener('mousemove', function(e) {
+                    if (!isMouseDown) return;
+                    e.preventDefault();
+                    const x = e.pageX - cardContainer.offsetLeft;
+                    const walk = (x - startX) * 1; // Ajuste conforme necessário para a sensibilidade de rolagem
+                    cardContainer.scrollLeft = scrollLeft - walk;
+                });
+            });
         });
 
-        cardContainer.addEventListener('mouseleave', () => {
-            isMouseDown = false;
-        });
-
-        cardContainer.addEventListener('mouseup', () => {
-            isMouseDown = false;
-        });
-
-        cardContainer.addEventListener('mousemove', (e) => {
-            if (!isMouseDown) return;
-            e.preventDefault();
-            const x = e.pageX - cardContainer.offsetLeft;
-            const walk = (x - startX) * 1; // Ajuste conforme necessário para a sensibilidade de rolagem
-            cardContainer.scrollLeft = scrollLeft - walk;
-        });
 
     </script>
 
